9bd92984764f8e87108b7c7f6f82f36ac484bac8
Fabio Morais <rarinaldo@outlook.com>
2023-10-04 17:43:08 -0300

    commiteifds

--- teatro.java
+++ teatro.java
@@ -18,13 +18,14 @@ public class teatro {
     static Scanner input = new Scanner(System.in);
 
     // variaveis globais
+    static boolean isSenhaDigitada = false;
     static int senhaAdministrador = 123;
     static float precoIngresso = -1;
     static float precoIngressoEstudante = -1;
     static float precoPadrao = 50;
     static String[][] teatro = new String[15][15];
-    static String arquivoTeatro = "teatro.txt";
-    static String arquivoPreco = "preco.txt";
+    static String NomeArquivoTeatro = "teatro.txt";
+    static String NomeArquivoPreco = "preco.txt";
     static String lugarLivre = "_";
     static String lugarOcupado = "X";
     static String lugarEstudante = "E";
@@ -38,11 +39,15 @@ public class teatro {
             e.printStackTrace();
         }
 
-        menuPrinciapl();
+        try {
+            menuPrinciapl();
+        } catch (Exception e) {
+
+        }
 
     }
 
-    public static void menuPrinciapl() {
+    public static void menuPrinciapl() throws Exception {
         boolean continuar = true;
         int tentativas = 0;
 
@@ -83,18 +88,18 @@ public class teatro {
     // rotinas de execução
     public static void rotina_lugares() throws Exception {
 
-        if (verificarArquivoExistente(arquivoTeatro)) {
-            teatro = lerTeatro(arquivoTeatro, teatro);
+        if (verificarArquivoExistente(NomeArquivoTeatro)) {
+            teatro = lerTeatro(NomeArquivoTeatro, teatro);
         } else {
             teatro = preencherNovoTeatro(teatro);
-            gravarTeatro(teatro, arquivoTeatro);
+            gravarTeatro(teatro, NomeArquivoTeatro);
         }
     }
 
     public static void rotina_preco() throws Exception {
 
-        if (verificarArquivoExistente(arquivoPreco)) {
-            precoIngresso = getPreco(arquivoPreco);
+        if (verificarArquivoExistente(NomeArquivoPreco)) {
+            precoIngresso = getPreco(NomeArquivoPreco);
             precoIngressoEstudante = precoIngresso / 2f;
         } else {
             setPreco(precoPadrao);
@@ -149,7 +154,7 @@ public class teatro {
     }
 
     public static float getPreco(String nomeArquivo) throws FileNotFoundException {
-        File ab = new File(arquivoPreco);
+        File ab = new File(NomeArquivoPreco);
         Scanner importador = new Scanner(ab);
         float numero = Float.parseFloat(importador.nextLine());
         importador.close();
@@ -160,7 +165,7 @@ public class teatro {
     public static void setPreco(float preco) throws Exception {
         precoIngresso = preco;
         precoIngressoEstudante = precoIngresso / 2f;
-        FileWriter w = new FileWriter(arquivoPreco);
+        FileWriter w = new FileWriter(NomeArquivoPreco);
         w.write(preco + "");
         w.close();
 
@@ -219,9 +224,9 @@ public class teatro {
 
         Digitar("atualmente temos: ");
         Digitar(contadorDeAssentos(lugarLivre) + " lugares livres");
-        Digitar(contadorDeAssentos(lugarOcupado) + " lugares ocupados");
+        Digitar((contadorDeAssentos(lugarOcupado) + contadorDeAssentos(lugarEstudante)) + " lugares ocupados");
         Digitar(contadorDeAssentos(lugarReservado) + " lugares reservados");
-        Digitar(contadorDeAssentos(lugarEstudante) + " lugares de estudante");
+        // Digitar(contadorDeAssentos(lugarEstudante) + " lugares de estudante");
 
     }
 
@@ -246,49 +251,61 @@ public class teatro {
 
         for (int i = 0; i < teatro.length; i++) {
             for (int j = 0; j < teatro[i].length; j++) {
-                Digitar(teatro[i][j] + " ");
+                System.out.print(teatro[i][j] + " ");
             }
+            PulaLinha();
         }
     }
 
+    public static void exibirRelatorioTeatro() {
+        PulaLinha();
+        exibirEstatisticasAssentos();
+        PulaLinha();
+        exibirNEstudantes();
+        PulaLinha();
+        exibirFaturamentoTotal();
+        ;
+        PulaLinha();
+        exibirFaturamentoEstudantes();
+        PulaLinha();
+    }
+
     // menu de ADM
-    public static void menuADM() {
+    public static void menuADM() throws Exception {
         boolean continuar = true;
         while (continuar) {
+
             if (senhaADM()) {
                 Digitar("Bem vindo Administrador do sistema");
                 Digitar("qual ação deseja realziar ?Digite o numero da opção");
-                Digitar("1 para vizualizar quantidades totais de lugares livres, reservados e confirmados");
-                Digitar("2 numero total de ingressos de estudantes");
-                Digitar("3 valor total arrecadado com os ingressos.");
-                Digitar("4 visualizar mapa de assentos");
+                Digitar("1 modificar o valor do ingresso");
+                Digitar("2 vizualizar o mapa de assentos");
+                Digitar("3 fornecer o relátorio do teatro");
                 Digitar("0 para sair do mono ADMINISTRADOR");
                 int opcao = input.nextInt();
 
                 switch (opcao) {
                     case 1:
-                        exibirEstatisticasAssentos();
+                        MenuModificarValorIngresso();
                         break;
 
                     case 2:
-                        exibirFaturamentoEstudantes();
+                        exibirMapaTeatro();
                         break;
 
                     case 3:
-                        exibirFaturamentoTotal();
-                        break;
-
-                    case 4:
-                        //
+                        exibirRelatorioTeatro();
                         break;
 
                     case 0:
                         Digitar("saindo do menu adm");
                         PulaLinha();
                         continuar = false;
+                        break;
 
                     default:
                         Digitar("opção invalida");
+                        break;
 
                 }
 
@@ -297,7 +314,23 @@ public class teatro {
 
     }
 
+    public static void MenuModificarValorIngresso() throws Exception {
+        PulaLinha();
+
+        Digitar("o valor atual do ingresso é de " + precoIngresso);
+        Digitar("para qual valor você deseja atualizar ?");
+        float novopreco = input.nextFloat();
+
+        setPreco(novopreco);
+
+        Digitar("o valor foi atualizado com sucesso !");
+    }
+
     public static boolean senhaADM() {
+        if (isSenhaDigitada) {
+            return true;
+        }
+
         int tentativas = 0;
         boolean continuar = true;
         while (continuar) {
@@ -315,6 +348,7 @@ public class teatro {
 
             if (senhaDigitada == senhaAdministrador) {
                 Digitar("Acesso permitido");
+                isSenhaDigitada = true;
                 return true;
             } else if (senhaDigitada == 0) {
                 Digitar("voltando para o menu Principal");
@@ -331,7 +365,193 @@ public class teatro {
     }
 
     // menu cliente
-    public static void menuCliente() {
+    public static void menuCliente() throws Exception {
+        boolean continuar = true;
+        while (continuar) {
+            Digitar("Bem vindo CLIENTE do sistema");
+            Digitar("qual ação deseja realziar ?Digite o numero da opção");
+            Digitar("1 vizualizar o mapa de assentos");
+            Digitar("2 reservar o assento");
+            Digitar("3 confimar uma reserva");
+            Digitar("0 para sair do mono ADMINISTRADOR");
+            int opcao = input.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    exibirMapaTeatro();
+                    break;
+
+                case 2:
+                    MenuReservarAssento();
+                    break;
+
+                case 3:
+                    MenuConfirmarReserva();
+                    break;
+
+                case 0:
+                    Digitar("saindo do menu adm");
+                    PulaLinha();
+                    continuar = false;
+                    break;
+
+                default:
+                    Digitar("opção invalida");
+                    break;
+
+            }
+        }
+
+    }
+
+    public static void MenuConfirmarReserva() throws Exception {
+        PulaLinha();
+        Digitar("qual assento vc deseja confirmar ? digite a linha e coluna");
+        int linha = input.nextInt();
+        int coluna = input.nextInt();
+
+        if (verificarRegrasConfirmacao(linha, coluna)) {
+            Digitar("confirmacao feita com sucesso");
+        } else {
+            Digitar("a confirmacao falhou ");
+        }
+
+    }
+
+    public static void MenuReservarAssento() throws Exception {
+        PulaLinha();
+        Digitar("Qual assento vc deseja reservar ? digite a linha e coluna");
+        int linha = input.nextInt();
+        int coluna = input.nextInt();
+
+        if (verificarRegrasReservas(linha, coluna)) {
+            Digitar("reserva feita com sucesso ^^");
+        } else {
+            Digitar("reserva falhou !");
+        }
+    }
+
+    public static boolean verificarRegrasConfirmacao(int l, int c) throws Exception {
+         if (verificarAssentoExiste(l, c)) {
+            // assento existe
+        } else {
+            Digitar("Assento nao existe");
+            return false;
+        }
+
+        if (VerificarAssentoReservado(l, c)) {
+            // lugar esta reservado
+        } else {
+            Digitar("assento nao esta reservado");
+            return false;
+        }
+
+        if(MenuCOnfirmarPagamento(l, c)) {
+            //pagamento efetuado
+        } else{
+            Digitar("Pagamento nao realizado");
+            return false;
+        }
+
+    
+
+
+
+        PulaLinha();
+        Digitar( gre +"CONFIMARCAO FEITA COM SUCESSO" + rst);
+        PulaLinha();
+
+        gravarTeatro(teatro, NomeArquivoTeatro);
+
+        return true;
+    }
+
+    public static void ConfirmarAssento(int l, int c, String estado) {
+        teatro[l][c] = estado;
+    }
+
+    public static boolean verificarRegrasReservas(int l, int c) throws Exception {
+
+        if (verificarAssentoExiste(l, c)) {
+            // assento existe
+        } else {
+            Digitar("Assento nao existe");
+            return false;
+        }
+
+        if (VerificarAssentoLivre(l, c)) {
+            // lugar esta livre
+        } else {
+            Digitar("assento nao esta livre");
+            return false;
+        }
+
+        reservarAssento(l, c);
+        PulaLinha();
+        Digitar( gre + "COMFIRMACAO FEITA COM SUCESSO" + rst);
+        PulaLinha();
+
+        gravarTeatro(teatro, NomeArquivoTeatro);
+
+
+
+        return true;
+    }
+
+    public static boolean VerificarAssentoReservado(int l, int c) {
+         if (teatro[l][c].equals(lugarReservado)) {
+            return true;
+        }
+
+         return false;
+    }
+
+    public static void reservarAssento(int l, int c) {
+        teatro[l][c] = lugarReservado;
+    }
+
+    public static boolean verificarAssentoExiste(int l, int c) {
+        if (l <= teatro.length && c <= teatro[0].length) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean VerificarAssentoLivre(int l, int c) {
+
+        if (teatro[l][c].equals(lugarLivre)) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    public static boolean MenuCOnfirmarPagamento(int l, int c) {
+        Digitar("deseja comfimar o pagamento");
+        Digitar("0 para não pagar");
+        Digitar("1 para pagar inteira");
+        Digitar("2 para pagar meia");
+        int resposta = input.nextInt();
+
+        switch(resposta) {
+            case 0:
+            return false;
+            
+
+            case 1:
+            ConfirmarAssento(l, c, lugarOcupado);
+            return true;
+
+            case 2:
+            ConfirmarAssento(l, c, lugarEstudante);
+            return true;
+
+            default:
+            return false;
+        }
+
 
     }
 
